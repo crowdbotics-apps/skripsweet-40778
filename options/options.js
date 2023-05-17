6ea4a3de04c8abf6d79f419f63a3d81f3d46c49c
jisung jongho <jisungjongho@gmail.com>
2023-05-18 00:57:40 +0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "skripsweet-40778",
   url: "https://skripsweet-40778.botics.co",
   api: "https://skripsweet-40778.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
